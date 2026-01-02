4fc55ad707d36a78378be332675575e0bef73d31
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-02 21:11:53 +0530

    Update create_tables.sql

--- sql/database1/create_tables.sql
+++ sql/database1/create_tables.sql
@@ -1,1 +1,11 @@
+USE database1;
 
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100)
+);
+
+INSERT INTO users (username, email) VALUES
+('alice', 'alice@example.com'),
+('bob', 'bob@example.com');
