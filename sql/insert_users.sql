01117141788ff5e78cdb2ed69fc4dbb7eb5d8249
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-05 18:49:19 +0530

    Update insert_users.sql

--- sql/insert_users.sql
+++ sql/insert_users.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (username, email) VALUES ('alice', 'alice@example.com');
-INSERT INTO users (username, email) VALUES ('bob', 'bob@example.com');
+INSERT INTO users (username, email) VALUES ('mariyababu', 'mariyababu@enreap.com');
+INSERT INTO users (username, email) VALUES ('avinash', 'avinash@enreap.com');
